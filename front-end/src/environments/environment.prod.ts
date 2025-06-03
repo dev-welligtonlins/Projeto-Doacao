f2c678fbe7ff1d6cae88e89a01c840a37c7aa10c
dev-welligtonlins <welligtonlinsemp@gmail.com>
2025-06-03 11:39:09 -0300

    environment.prod url render + cors

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { IEnviroment } from "./enviroment.interface";
 
 export const environment: IEnviroment = {
   production: true,
-  API_URL: 'https://projeto-doacao.onrender.com/',
-  FRONT_URL: 'https://doacaoplus.netlify.app'
+  API_URL: 'https://projeto-doacao.onrender.com',
+  FRONT_URL: 'https://front-end-doacao.onrender.com'
 };  
